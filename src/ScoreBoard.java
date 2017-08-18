11a15d678e23cb09ee0531c7e668cadae6422ac4
chaitanya duse <chaitanya@wearexenon.com>
2017-08-18 08:41:11 +0530

    First commit

--- src/ScoreBoard.java
+++ src/ScoreBoard.java
@@ -4,8 +4,6 @@ public class ScoreBoard {
         for (int i = 0; i < 100000; i++) {
             System.out.println(TotalScore.getInsance().getsCore());
             TotalScore.getInsance().addToscore(1);
-
-
         }
     }
 }
